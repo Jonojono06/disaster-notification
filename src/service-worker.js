cfaac1f9fca179214479e04ad38978a06d345d30
Johno <Jono@wardz.ca>
2025-03-01 18:50:37 -0700

    update service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,23 @@
+// import { precacheAndRoute } from 'workbox-precaching';
+
+// precacheAndRoute(self.__WB_MANIFEST);
+
+// self.addEventListener('push', (event) => {
+//   const data = event.data.json();
+//   self.registration.showNotification(data.title, {
+//     body: data.body,
+//     icon: '/logo192.png',
+//   });
+// });
+
+// self.addEventListener('notificationclick', (event) => {
+//   event.notification.close();
+//   event.waitUntil(clients.openWindow('/'));
+// });
+
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+
 import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
